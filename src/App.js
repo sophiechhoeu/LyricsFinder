c09fb65956b923ec9e030d542ff5f45f437de8f9
sophie.chhoeu <sophie.chhoeu@carsguide.com.au>
2018-05-12 19:37:03 +1000

    updated to search function, catch error and form field

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
@@ -6,28 +7,57 @@ class App extends Component {
     super();
     this.state={
       data:[],
+      artist:'',
+      title: ''
     }
   }
 
+ //  componentDidMount(){
+ //   fetch(`https://api.lyrics.ovh/v1/TLC/Unpretty`)
+ //   .then(res =>res.json())
+ //   .then((json) => {
+ //     console.log(json.lyrics)
+ //     console.log(json.error)
+ //     this.setState({
+ //       data:[json.lyrics,json.error]
+ //       })
+ //   });
+ // }
 
-
-  componentDidMount(){
-    fetch(`https://api.lyrics.ovh/v1/TLC/Unpretty`)
+  search(){
+    fetch(`https://api.lyrics.ovh/v1/${this.state.artist}/${this.state.title}`)
     .then(res =>res.json())
     .then((json) => {
-      console.log(json.lyrics)
-      console.log(json.error)
       this.setState({
         data:[json.lyrics,json.error]
         })
-    });
+    })
+    .catch(function(error) { console.log( error); });
   }
 
 
   render() {
     return (
       <div className="App">
-        <h1>{this.state.data}</h1>
+        <h1>LyricFinder</h1>
+       <div className="search-form">
+        <Form>
+          <FormGroup className="artist-search">
+            <Label id="artist" for="artist">Artist</Label>
+            <Input type="artist" name="artist" id="Artist" placeholder="Artist"
+              value={this.state.artist}
+              onChange={event => {this.setState({artist:event.target.value})}}/>
+          </FormGroup>
+          <FormGroup>
+            <Label for="title">Title</Label>
+            <Input type="title" name="title" id="Title" placeholder="Title" value={this.state.title}
+            onChange={event => {this.setState({title:event.target.value})}}/>
+          </FormGroup>
+        <Button onClick={() => this.search()}>Submit</Button>
+          </Form>
+          </div>
+        <h2>{this.state.data}</h2>
+
       </div>
     );
   }
